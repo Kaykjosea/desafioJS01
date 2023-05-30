a298963b96798497b7a2286ea30b344a4165948f
Kaykjosea <129893557+Kaykjosea@users.noreply.github.com>
2023-05-30 16:09:18 -0300

    Add files via upload

---
+++ codigos.js
@@ -0,0 +1,23 @@
+
+
+
+function ativarNome(){
+    let nome = 'savio lindo';
+    console.log(nome)
+     
+    document.getElementById('nome').innerHTML = nome
+
+}
+
+
+function somarNumeros(){
+    let a = 2;
+    let b = 3.5;
+    let c = a*b;
+
+
+    console.log(c)
+    
+    document.getElementById('aparecer').innerHTML = c
+}
+
